791a9b24335999c125cea500872bcf599b0732ba
scook0002 <scook0002@yahoo.com>
2023-03-12 19:02:47 -0500

    Changed class names in css and html

--- js/index.js
+++ js/index.js
@@ -6,8 +6,22 @@ $(document).ready(function(){
 });
 
 
+$(".grow").mouseover(function(){
+    $(this).css("color","#3D8EBC");
+}).mouseout(function(){
+    $(this).css("color","#94989C");
+});
 
-// Intersection Observer 
+
+  $(".image-container").mouseover(function() {    
+    $(this).attr("src", $(this).attr("src").replace("Logo.png", "home.png"));
+}).mouseout(function() {
+    $(this).attr("src", $(this).attr("src").replace("home.png", "Logo.png"));
+});
+
+
+
+// Intersection Observer (Lazy Loading of My Skills & My Works)
 const faders = document.querySelectorAll(".fade-in");
 
 const appearOptions = {
