d44172c2ec23bf3681d52675bca957a7694a053e
scook0002 <scook0002@yahoo.com>
2023-03-15 15:50:49 -0500

    Before iterations, added js button

--- js/index.js
+++ js/index.js
@@ -5,22 +5,26 @@ ScrollReveal().reveal('.Intro', { delay: 700 });
 ScrollReveal().reveal('.welcome', { delay: 900 });
 ScrollReveal().reveal('.outline', { delay: 1100 });
 
-ScrollReveal().reveal('.lightHeading', { delay: 1300 });
-ScrollReveal().reveal('.dots', { delay: 1500 });
-ScrollReveal().reveal('.MySkills', { delay: 1700 });
-ScrollReveal().reveal('.dots2', { delay: 1900 });
+function myFunction() {
+    document.getElementById("aboutMe").innerHTML = "Learn more about me!";
+}
 
-ScrollReveal().reveal('.myTools', { delay: 2100 });
-ScrollReveal().reveal('.darkHeading', { delay: 2300 });
+ScrollReveal().reveal('.lightHeading', { delay: 500 });
+ScrollReveal().reveal('.dots', { delay: 700 });
+ScrollReveal().reveal('.MySkills', { delay: 900 });
+ScrollReveal().reveal('.dots2', { delay: 1100 });
 
-ScrollReveal().reveal('.row1', { delay: 2500 });
-ScrollReveal().reveal('.mockup', { delaay: 2700 });
-ScrollReveal().reveal('.row2', { delay: 2900 });
-ScrollReveal().reveal('.mockup', { delay: 3100 });
-ScrollReveal().reveal('.row3', { delay: 3300 });
-ScrollReveal().reveal('.mockup', { delaay: 37000 });
+ScrollReveal().reveal('.myTools', { delay: 500 });
+ScrollReveal().reveal('.darkHeading', { delay: 700 });
 
-ScrollReveal().reveal('.section4', { delay: 3900 });
-ScrollReveal().reveal('.backtotop', { delay: 4100 });
-ScrollReveal().reveal('.footertext', { delay: 4300 });
-ScrollReveal().reveal('.contact', { delay: 4500 });
+ScrollReveal().reveal('.row1', { delay: 500 });
+ScrollReveal().reveal('.mockup', { delay: 700 });
+ScrollReveal().reveal('.row2', { delay: 900 });
+ScrollReveal().reveal('.mockup', { delay: 1100 });
+ScrollReveal().reveal('.row3', { delay: 1300 });
+ScrollReveal().reveal('.mockup', { delay: 1700 });
+
+ScrollReveal().reveal('.section4', { delay: 500 });
+ScrollReveal().reveal('.backtotop', { delay: 700 });
+ScrollReveal().reveal('.footertext', { delay: 900 });
+ScrollReveal().reveal('.contact', { delay: 1100 });
